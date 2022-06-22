98a951c6b63e99a904b8b1b7c0b527b08e601a17
Cristian66333 <74688238+Cristian66333@users.noreply.github.com>
2022-06-22 18:34:22 -0500

    login

---
+++ scripts/uptc.edu.co.model/Management.js
@@ -0,0 +1,5 @@
+function obtenerCredenciales(){
+    var usuario= document.getElementById("usuario").value
+    var contrasena= document.getElementById("contrasena").value
+    console.log("usuario: "+usuario+" contraseña"+contrasena)
+}
