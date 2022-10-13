1a9a5734066520e2dde7a35c744e06c241ef1139
Laura-Barrera <laura.barrera14@uptc.edu.co>
2022-10-13 09:36:25 -0500

    Avance 1 modf

--- scripts/uptc.edu.co.model/PHP/approveCancel.php
+++ scripts/uptc.edu.co.model/PHP/approveCancel.php
@@ -5,4 +5,12 @@ $idCancel=$_POST["numCancelacion"];
 $updateCancel = "update cancelacion set estadoCancelacion='Aprobado'";
 $result = $conn->query($updateCancel);
 
+$codigoPresta = "select codigoPrestamo from cancelacion where numCancelacion = '$idCancel'";
+$result1 = $conn->query($codigoPresta);
+
+$updatePrestamo = "update solicitud_prestamo set estado = 'Cancelado' where codigoPrestamo = '$result1'";
+$result2 = $conn->query($updatePrestamo);
+
+
+
 
