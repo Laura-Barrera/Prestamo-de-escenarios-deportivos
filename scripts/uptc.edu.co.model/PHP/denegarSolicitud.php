7a1c891cfdaa61e96e59bc9a8c29d0dbc41888d6
Cristian66333 <74688238+Cristian66333@users.noreply.github.com>
2022-11-12 18:57:33 -0500

    cambio palabra

--- scripts/uptc.edu.co.model/PHP/denegarSolicitud.php
+++ scripts/uptc.edu.co.model/PHP/denegarSolicitud.php
@@ -4,6 +4,6 @@ $idSolicitud=$_POST['idSolicitud'];
 
 require('connection.php');
 
-$query="update solicitud_prestamo set estado='Cancelado' where codigoSolicitud='$idSolicitud'";
+$query="update solicitud_prestamo set estado='No Autorizado' where codigoSolicitud='$idSolicitud'";
 $result=$conn->query($query);
 echo $result;
